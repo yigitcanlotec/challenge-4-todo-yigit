320dbf1af21204db4ba4bd8113f68d0194e99c97
YigitJohn <yigitcanustek@gmail.com>
2023-10-29 18:18:52 +0300

    Changed Vite configs.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   build: {
     rollupOptions: {

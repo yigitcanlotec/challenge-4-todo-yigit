e4b69c0b42e3e783fc8ba080444b7742f73ce27f
Yigit <yigitcan.ustek@lotec.com.tr>
2023-10-30 09:58:58 +0300

    Bugfix.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './',
-  build: {
-    outDir: 'dist',
-  },
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: '/public/index.html',
-    },
+    outDir: 'dist',
   },
 });

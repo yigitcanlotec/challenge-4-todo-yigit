c81728d96589800fcaf66e69c7ee5918a146d5e1
Yigit <yigitcan.ustek@lotec.com.tr>
2023-11-09 09:16:40 +0300

    Updated package.json in Client.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import resolve from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    resolve({
-      extensions: ['.mjs', '.js', '.json', '.node', '.jsx', '.tsx'], // Add .jsx here if needed
-    }),
-  ],
+  plugins: [react()],
 
   build: {
     outDir: 'dist',
